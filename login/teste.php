3b919489740c6b2b540bad7f62d53c674ac998a4
Raissa <raissa.santos171@etec.sp.gov.br>
2025-04-25 15:25:32 -0300

    Fiz a conexão do formulário com o banco de dados

--- login/teste.php
+++ login/teste.php
@@ -5,20 +5,20 @@ require "Usuario.class.php";
 $exito = $usuario = new Usuario();
 
 if($exito){
-    /*
-    $nome = "Girafarles";
-    $email = "girafarles@gmail.com";
-    $senha = "123456";
+   if(isset($_POST["cadastrar"])){
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
 
-    $usuario->cadastrar($nome, $email, $senha);*/
-    
+        $usuario->cadastrar($nome, $email, $senha);
+
+   }else{
+        echo "<script>
+      confirm('Erro ao conectar. Tente mais tarde')
+        <script>";
+   }     
 }
-else{
-    echo "<script>
-    confirm('Erro ao conectar. Tente mais tarde')
-    <script>";
-    
-}
+
 
 
 
